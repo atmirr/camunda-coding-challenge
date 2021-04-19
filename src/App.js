83ab00cf0efd708ae40a921f46d9c7e8d4f48477
Amir Attari <amir.aetari@gmail.com>
2021-04-19 11:35:57 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// import Navigation component
+// import HomePage component
+// import ModelerPage component
+
+export default () => (
+  <div>
+    {/* render a navigation component */}
+    <hr />
+    {/* render routing logic of react-router-dom */}
+  </div>
+);
